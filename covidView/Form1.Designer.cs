75210300e5367f264ce37b65587cc69b90121b90
Logic-01001010 <dnwn306@naver.com>
2022-02-02 10:32:09 +0900

    first commit

---
+++ covidView/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace covidView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼옛체", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(467, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "코로나바이러스감염증-19 국내 발생현황 (02.01. 00시 기준)";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("휴먼둥근헤드라인", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(51, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 49);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0000명";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("휴먼둥근헤드라인", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(51, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(391, 49);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0000명";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "전체 확진자";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "하루 확진자";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(352, 532);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "마지막 갱신: ";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(168, 552);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(273, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "2022년 02월02일 99시 99분 99초\r\n";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::covidView.Properties.Resources.black_refresh_icon_png_9;
+            this.pictureBox1.Location = new System.Drawing.Point(420, 512);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(33, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 209);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "전국";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(33, 262);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(467, 256);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "지역";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "서울",
+            "경기",
+            "부산",
+            "인천",
+            "제주"});
+            this.comboBox1.Location = new System.Drawing.Point(357, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 28);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("휴먼둥근헤드라인", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label11.Location = new System.Drawing.Point(51, 97);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(391, 49);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "0000명";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("휴먼둥근헤드라인", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label9.Location = new System.Drawing.Point(51, 180);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(391, 49);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "0000명";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(343, 75);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 20);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "전체 확진자";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(343, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 20);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "하루 확진자";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(271, 27);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(223, 20);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "api by: https://api.corona-19.kr/";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(539, 610);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "covidView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ covidView/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace covidView
+{
+    public partial class Form1 : Form
+    {
+        private const string URL = "https://api.corona-19.kr/korea/beta/";
+        private string api = "?serviceKey={YOUR_APIKEY_HERE}";
+
+        string[] regions = { "seoul", "gyeonggi", "busan", "incheon", "jeju" };
+
+        JObject json;
+
+        private void reload()
+        {
+            
+            
+            label7.Text = DateTime.Now.ToString("yyyy년 MM월dd일 HH시 mm분 ss초");
+
+
+            using (WebClient wc = new WebClient())
+            {
+                string res = new WebClient().DownloadString(URL + api);
+
+                json = JObject.Parse(res);
+
+                string latest_update = json["API"]["updateTime"].ToString(); // 갱신시간
+                string total = json["korea"]["totalCnt"].ToString(); // 전체 확진자
+                string dayIncrease = json["korea"]["incDec"].ToString(); // 하루 증가
+
+
+                label1.Text = latest_update;
+                label2.Text = String.Format("{0:#,###}", Int32.Parse( total ) ) + "명";
+                label3.Text = String.Format("{0:#,###}", Int32.Parse( dayIncrease ) ) + "명";
+
+
+                string select_total = json[regions[comboBox1.SelectedIndex]]["totalCnt"].ToString(); // 전체 확진자
+                string select_dayIncrease = json[regions[comboBox1.SelectedIndex]]["incDec"].ToString(); // 하루 증가
+
+                label11.Text = String.Format("{0:#,###}", Int32.Parse(select_total)) + "명";
+                label9.Text = String.Format("{0:#,###}", Int32.Parse(select_dayIncrease)) + "명";
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
+
+            reload();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if( json != null) {
+                string total = json[regions[comboBox1.SelectedIndex]]["totalCnt"].ToString(); // 전체 확진자
+                string dayIncrease = json[regions[comboBox1.SelectedIndex]]["incDec"].ToString(); // 하루 증가
+
+                label11.Text = String.Format("{0:#,###}", Int32.Parse(total)) + "명";
+                label9.Text = String.Format("{0:#,###}", Int32.Parse(dayIncrease)) + "명";
+            }
+
+        }
+    }
+}
